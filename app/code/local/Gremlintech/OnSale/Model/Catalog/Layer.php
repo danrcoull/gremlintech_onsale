04abbbf33e45b0f837ebb9fec9df4b7a688f3d9a
gremlin <ttechitsolutions@gmail.com>
2016-08-20 16:42:23 +0100

    hotfix : error check handles

--- app/code/local/Gremlintech/OnSale/Model/Catalog/Layer.php
+++ app/code/local/Gremlintech/OnSale/Model/Catalog/Layer.php
@@ -9,27 +9,38 @@ class Gremlintech_OnSale_Model_Catalog_Layer extends Mage_Catalog_Model_Layer
             $collection = $this->_productCollections[$this->getCurrentCategory()->getId()];
         } else {
 
-            $collection = Mage::getResourceModel('catalog/product_collection')
-                ->addAttributeToSelect('*');
 
-            $todayDate = date('m/d/y');
-            $tomorrow = mktime(0, 0, 0, date('m'), date('d'), date('y'));
-            $tomorrowDate = date('m/d/y', $tomorrow);
 
-            /**$collection->addFieldToFilter('special_from_date', array('or'=> array(
-                0 => array('date' => true, 'from' => $tomorrowDate),
-                1 => array('is' => new Zend_Db_Expr('null')))
-            ))
-                ->addFieldToFilter('special_to_date', array('or'=> array(
-                    0 => array('date' => true, 'from' => $tomorrowDate),
-                    1 => array('is' => new Zend_Db_Expr('null')))
-                ), 'left')
-            ;**/
+            $handles = Mage::app()->getLayout()->getUpdate()->getHandles();
 
-            $collection->addPriceDataFieldFilter('%s < %s', ['final_price', 'price']);
+
+            if(in_array('category_sale',$handles)) {
+
+                $todayDate = date('m/d/y');
+                $tomorrow = mktime(0, 0, 0, date('m'), date('d'), date('y'));
+                $tomorrowDate = date('m/d/y', $tomorrow);
+
+                $collection = Mage::getResourceModel('catalog/product_collection')
+                        ->addAttributeToSelect('*');
+
+                /**$collection->addFieldToFilter('special_from_date', array('or'=> array(
+                 * 0 => array('date' => true, 'from' => $tomorrowDate),
+                 * 1 => array('is' => new Zend_Db_Expr('null')))
+                 * ))
+                 * ->addFieldToFilter('special_to_date', array('or'=> array(
+                 * 0 => array('date' => true, 'from' => $tomorrowDate),
+                 * 1 => array('is' => new Zend_Db_Expr('null')))
+                 * ), 'left')
+                 * ;**/
+
+                $collection->addPriceDataFieldFilter('%s < %s', ['final_price', 'price']);
+            }else
+            {
+                $collection = $this->getCurrentCategory()->getProductCollection();
+            }
             $this->prepareProductCollection($collection);
             $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
-}
+        }
         return $collection;
 
 
